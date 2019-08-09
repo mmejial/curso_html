53e3a119a7c1730777e24296d0c3fc419ece5ff2
Marco <tomatin77@gmail.com>
2019-08-09 10:53:43 -0500

    nuevos

---
+++ js/hola-mundo/scripts/hola.js
@@ -0,0 +1,14 @@
+alert("hola mundo");
+document.write("el taquito de pollo");
+var uno = Number(prompt("introduce un nomero")); //variable global
+var dos = Number(prompt("introduce un nomero"));
+let tres = 2;
+let cuatro = 2;
+//let dos = 5; variable local
+console.log(uno);
+console.log(dos);
+console.log(uno + dos);
+document.write(uno + dos);
+document.write(tres + cuatro);
+//typeof muestra el tipo de variables
+console.log(typeof tres);
