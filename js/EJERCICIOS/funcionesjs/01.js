afe0e4d55565ade3bac10ec9035330b3541f9f33
Marco <tomatin77@gmail.com>
2019-08-28 14:42:41 -0500

    modificados

---
+++ js/EJERCICIOS/funcionesjs/01.js
@@ -0,0 +1,12 @@
+'use strict'
+//
+
+function suma(a, b) {
+
+    console.log(a + " X " + b + " = " + a * b);
+
+}
+var x = Number(prompt("introduce un numero"));
+for (var i = 0; i <= 10; i++) {
+    suma(i, x);
+}
